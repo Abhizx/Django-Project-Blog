ed0d4a9c7facc9df820b22d4c9bb9611b1b888e0
Abhinav <mr.rocker.technology@gmail.com>
2021-02-07 19:02:30 +0530

    make it better

--- home/models.py
+++ home/models.py
@@ -11,5 +11,5 @@ class Contact(models.Model):
     timeStamp = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
-        return 'Message from ' + self.name + ' at ' + self.timeStamp
+        return 'Message from ' + self.name + ' at ' + str(self.timeStamp)
     
